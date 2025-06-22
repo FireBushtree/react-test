b0d70af32cd4c4babfd48c3977bbebe86152a143
huangqian <huangqian@chinacici.com>
2025-06-22 08:17:11 +0800

    init repo

---
+++ scripts/vite/vite.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+  ],
+
+  resolve: {
+    alias: [
+      {
+        find: 'react',
+        replacement: resolvePkgPath('react')
+      },
+      {
+        find: 'react-dom',
+        replacement: resolvePkgPath('react-dom')
+      },
+      {
+        find: 'hostConfig',
+        replacement: path.resolve(resolvePkgPath('react-dom'), './src/hostConfig.js')
+      }
+    ]
+  },
+})
